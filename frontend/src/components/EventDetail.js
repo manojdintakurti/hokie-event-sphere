dd30b0970d435f17838c30d7b2c8d2efbc3d8ef3
saimanoj <saimanoj@vt.edu>
2024-10-12 01:40:25 +0000

    event details page

---
+++ frontend/src/components/EventDetail.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../styles/EventDetail.css';
+import eventImage from '../Images/eventItem.png';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import EventDetailMid from "./EventDetailMid";
+import EventCatalog from "./EventCatalog"; // Add social media icons
+
+function EventDetail() {
+    return (
+        <div>
+            <div className="event-detail-page">
+                {/* Background image */}
+                <div className="background-overlay" style={{backgroundImage: `url(${eventImage})`}}></div>
+
+                {/* Back button at top left */}
+                <button className="back-button">← Back</button>
+
+                <div className="event-content">
+                    {/* Left side: Event heading and details */}
+                    <div className="event-info">
+                        <h1>Dream World Wide in Jakarta</h1>
+                        <p>Virginia Tech</p>
+                        <p>Description of the event goes here. It's suitable for both beginners and experienced
+                            users...</p>
+                    </div>
+
+                    {/* Right side: Date, Time, Location and RSVP */}
+                    <div className="event-details-box">
+                        <h4>Date & Time</h4>
+                        <p className="text-grayed">Saturday, March 18, 2023, 9:30 PM</p>
+
+                        <a className="add-to-calendar">Add to calendar</a>
+
+                        <button className="rsvp-button">RSVP for the Event</button>
+                    </div>
+                </div>
+            </div>
+            <EventDetailMid />
+            <span className={"event-details-suggestions-heading"}>Events you may Like</span>
+            <EventCatalog />
+        </div>
+
+    );
+}
+
+export default EventDetail;
