469dd4b3faa54c8167c07f6b5f994db6a24fa527
Shrihari Maheshwari <shrihari.maheshwari7@gmail.com>
2024-11-07 14:22:45 -0500

    added routes for categorizing the data

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,11 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(MONGODB_URI, {
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
   dbName: 'events_db', // Force connection to events_db
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log('Successfully connected to MongoDB Atlas - Database: events_db');
   // Log additional connection details
